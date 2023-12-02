22d7c2b9c810ea24317eef70a38d4a8d4ba97914
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2023-12-01 22:24:32 -0700

    Update functions.py

--- LoanY/functions.py
+++ LoanY/functions.py
@@ -1,50 +1,225 @@
+import json
+import requests
+import os
+import openai
+import toml
+from openai import OpenAI
+from prompts import assistant_instructions
+import streamlit as st
 
-assistant_instructions = """
-You are LoanY, a proactive loan officer aide bot designed to guide potential home buyers through the mortgage process. Your role involves providing detailed explanations about various mortgage types, such as fixed-rate, adjustable-rate (ARM), balloon, jumbo, and government-backed loans like FHA, VA, and USDA. 
-You've been given access to large VA and FHA pdfs.
-
-Your approach is engaging and user-friendly, focused on helping users make informed decisions about their mortgage choices. You can explain complex financial concepts in a straightforward manner, suitable for those without a financial background.
-
-At the start of each interaction, you start by saying, “Hello, I am LoanY, an aide to your loan officer, and I’ll be asking you a series of questions (about 20-30), providing relevant info along the way.  A summarization of this conversation will be sent to your loan officer”. 
-Then, you will take the initiative to lead the conversation by asking a series of targeted questions one by one to better understand the user’s needs.  Start by asking the 17 questions below one by one.  Do not give them all 17 questions at once.  When they answer "no" you do not need to reassure them.
-
-Once they have answered the question giving their name, address them by that name.
-
-1. What is your first name and last initial?
-2. Are you considering a property in a rural area?
-If the user is interested in property in a rural area, encourage them to search for the property using this online tool: https://eligibility.sc.egov.usda.gov/eligibility/welcomeAction.do.  Then ask them the result of their search.
-3. Are you or a family member a current or former military service member?
-4. Could you share your total debt?
-5. Have you had any late payments or collections on debts in the last 12 months?
-6. What is you current credit score?
-7. What is your total yearly income?
-8. How much can you afford for a down payment?  The size of the down payment affects loan terms and interest rates, and it can determine the need for mortgage insurance.
-9. Have you been employed continuously for the past two years?  
-10. What type of property are you looking to buy?(e.g., single-family home, condo, multi-unit)
-11. Are you a first-time home buyer? First-time buyers may be eligible for special programs or incentives.
-12. Have you ever filed for bankruptcy or had a foreclosure?
-13. What are your expectations regarding interest rate and monthly payment amount?
-14. Do you have any savings set aside for emergencies?  If so, how much? Lenders prefer borrowers with a financial cushion to cover unexpected expenses.
-15. Are you working with a real estate agent?
-16. When are you hoping to buy a home?
-17. Are you aware of all the costs involved in buying a home? (e.g. closing costs, insurance, taxes, and maintenance expenses.)
+# GMAIL API
+import base64
+from email.message import EmailMessage
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
-Once the above 17 questions have been asked say, "That’s it for the first part.  In this next part, I may require some think time between responses so please bear with me.”
+# Load configuration from config.toml
+config = toml.load('/Users/cameronhightower/Documents/LoanY/.streamlit/config.toml')
 
-Based on their responses, you will lead the conversation, asking follow-up questions and providing information tailored to their specific situation. You will highlight key features, pros, cons, and any specific requirements of different mortgage types. Tailor your responses to the specific needs and questions of the user, ensuring they get the most relevant and accurate information for their situation. 
+# Set the OpenAI and GMAIL API keys from the config file
+openai.api_key = config['openai']['api_key']
+OPENAI_API_KEY = openai.api_key
+GMAIL_API_KEY = config['gmail']['api_key']
 
-If a conventional loan is the best option, ask them if they know the difference between fixed and ARM.
 
-If the user is an active military service member, veteran, or eligible family member, thus qualifying for a VA loan, ask the user a minimum of 4 follow up questions, one by one, referencing the VA_pamphlet pdf that you have access to to inform your questions.  Do not ask all 4 questions at once.  This VA pamphlet was not provided by the user; you already have access to it.
 
-If the user is eligible for an FHA loan, ask the user a minimum of 4 follow up questions, one by one, referencing the FHA_pamphlet pdf to inform your questions.  Do not ask all 4 questions at once.  This FHA document is not provided by the user; you already have access to it.
+# Init OpenAI Client
+if 'client' not in st.session_state:
+    st.session_state.client = OpenAI(api_key = OPENAI_API_KEY)
 
-Once all relevant questions have been asked, create a summary of the conversation with the prospective buyer.  This summary
-should include the key points discussed, any specific advice given, and a list of the mortgage options that were explored.  Format the summary in a way that is usable for downstream use, utlizing new lines, titles, bullets, and numbering while keeping the data type a string.
-Send an email to the human loan officer by passing the summary and client_name (the users name) to the gmail_send_message function.
-Ask the user "Is there anything else I can help you with?"
-If they say no, send the email summary.
-Finally, thank the user for answering the questions and wish them well.
 
-"""
+SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
+
+# Send a summary email to the human loan officer
+def gmail_send_message(client_name, summary):
+  """Create and send an email message
+  Print the returned  message id
+  Returns: Message object, including message id
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    message = EmailMessage()
+
+    message.set_content(summary)
+
+    message["To"] = "cameron.hightower@simbabuilds.com"
+    message["From"] = "cameron.hightower@simbabuilds.com"
+    message["Subject"] = f"Summary of conversation with {client_name}"
+
+    # encoded message
+    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+    create_message = {"raw": encoded_message}
+    # pylint: disable=E1101
+    send_message = (
+        service.users()
+        .messages()
+        .send(userId="me", body=create_message)
+        .execute()
+    )
+    print(f'Message Id: {send_message["id"]}')
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    send_message = None
+  return send_message
+
+
+
+
+
+
+# # Add lead to Airtable
+# def create_lead(name, phone, address):
+#   url = "https://api.airtable.com/v0/appM1yx0NobvowCAg/Leads"  # Change this to your Airtable API URL
+#   headers = {
+#       "Authorization": AIRTABLE_API_KEY,
+#       "Content-Type": "application/json"
+#   }
+#   data = {
+#       "records": [{
+#           "fields": {
+#               "Name": name,
+#               "Phone": phone,
+#               "Address": address
+#           }
+#       }]
+#   }
+#   response = requests.post(url, headers=headers, json=data)
+#   if response.status_code == 200:
+#     print("Lead created successfully.")
+#     return response.json()
+#   else:
+#     print(f"Failed to create lead: {response.text}")
+
+
+
+
+# Create or load assistant
+def create_assistant(client):
+    assistant_file_path = 'assistant.json'
+
+    # If there is an assistant.json file already, then load that assistant
+    if os.path.exists(assistant_file_path):
+        with open(assistant_file_path, 'r') as file:
+            assistant_data = json.load(file)
+            assistant_id = assistant_data['assistant_id']
+            print("Loaded existing assistant ID.")
+    else:
+    # If no assistant.json is present, create a new assistant using the below specifications
+
+        fha_file = client.files.create(file=open("/Users/cameronhightower/Documents/LoanY/docs/FHA_pamphlet.pdf", "rb"),
+                               purpose='assistants')
+        
+        va_file = client.files.create(file=open("/Users/cameronhightower/Documents/LoanY/docs/VA_pamphlet.pdf", "rb"),
+                               purpose='assistants')
+
+        assistant = client.beta.assistants.create(
+        # Getting assistant prompt from "prompts.py" file, edit on left panel if you want to change the prompt
+        instructions=assistant_instructions,
+        model= "gpt-3.5-turbo-1106",
+        tools=[
+            {
+                "type": "retrieval"  # This adds the knowledge base as a tool
+            },
+            {
+                "type": "function",  # This adds sending an email as a tool
+                "function": {
+                    "name": "gmail_send_message",
+                    "description":
+                    "Sends an email to a human loan officer containing a summary of the conversation with the prospective buyer",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "summary": {
+                                "type":
+                                "string",
+                                "description":
+                                "Summary of conversation with client"
+                            },
+                            "client_name": {
+                                "type":
+                                "string",
+                                "description":
+                                "The name of the client"
+                            }
+                        },
+                        "required": ["summary", "client_name"]
+                    }
+                }
+            }
+        #     ,{
+        #         "type": "function",  # This adds the lead capture as a tool
+        #         "function": {
+        #             "name": "create_lead",
+        #             "description":
+        #             "Capture lead details and save to Airtable.",
+        #             "parameters": {
+        #                 "type": "object",
+        #                 "properties": {
+        #                     "name": {
+        #                         "type": "string",
+        #                         "description": "Name of the lead."
+        #                     },
+        #                     "phone": {
+        #                         "type": "string",
+        #                         "description": "Phone number of the lead."
+        #                     },
+        #                     "address": {
+        #                         "type": "string",
+        #                         "description": "Address of the lead."
+        #                     }
+        #                 },
+        #                 "required": ["name", "phone", "address"]
+        #             }
+        #         }
+        #     }
+        ],
+        file_ids=[fha_file.id, va_file.id])
+
+        # Create a new assistant.json file to load on future runs
+        with open(assistant_file_path, 'w') as file:
+            json.dump({'assistant_id': assistant.id}, file)
+            print("Created a new assistant and saved the ID.")
+
+        assistant_id = assistant.id
+
+    return assistant_id
+
+
+
+
+
+
+
+
+
+
